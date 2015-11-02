d01cbb7b52fdb5eba5b62c9de77850019604664b
Steven Burgess <sburgess@datto.com>
2015-11-02 14:43:57 -0500

    Moar Fizxz

--- Pid.py
+++ Pid.py
@@ -6,18 +6,21 @@ import sys
 
 pid_file_path = '/var/run/zfs_perf_test.pid'
 
+
 def create_pid_file():
     if pid_file_exists():
         print("Pid file exists, please ensure that no other instances of " +
-        "these performance tests are running.")
+              "these performance tests are running.")
         sys.exit(1)
     pid = os.getpid()
     pid_file_object = open(pid_file_path, 'w')
     pid_file_object.write(str(pid))
     pid_file_object.close()
 
+
 def destroy_pid_file():
     os.unlink(pid_file_path)
+
 
 def pid_file_exists():
     return os.path.exists(pid_file_path)
